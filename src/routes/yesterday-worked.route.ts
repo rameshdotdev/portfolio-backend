fa210778e8703c85fa3d1ce662e6512690a299ab
rameshdotin <ramesh2041999@gmail.com>
2026-03-07 11:39:05 +0530

    fixes

--- src/routes/yesterday-worked.route.ts
+++ src/routes/yesterday-worked.route.ts
@@ -3,21 +3,21 @@ import axios from "axios";
 import { env } from "../config/env.js";
 import { cacheOrFetch } from "../utils/cache.js";
 
+import https from "https";
 const router = Router();
-
+const httpsAgent = new https.Agent({
+  family: 4,
+});
 const CACHE_TTL_SECONDS = 60 * 60 * 4; // 4 hours
-
 function toBasicAuth(apiKey: string) {
   return `Basic ${Buffer.from(apiKey + ":").toString("base64")}`;
 }
-
 async function fetchWakaTimeData() {
   const apiKey = env.WAKATIME_API_KEY;
   console.log("Fetching Api", apiKey);
   if (!apiKey) {
     throw new Error("Missing WAKATIME_API_KEY");
   }
-
   const { data } = await axios.get(
     "https://wakatime.com/api/v1/users/current/summaries",
     {
@@ -25,23 +25,19 @@ async function fetchWakaTimeData() {
       headers: {
         Authorization: toBasicAuth(apiKey),
       },
+      httpsAgent,
     },
   );
-
   console.log("Fetching Data", data);
   const days = data?.data ?? [];
   const yesterday = days?.[0];
   const editors = yesterday?.editors ?? [];
-
   const wanted = new Set(["Cursor", "VS Code"]);
-
   const filteredEditors = editors.filter((e: any) => wanted.has(e.name));
-
   const combinedSeconds = filteredEditors.reduce(
     (sum: number, e: any) => sum + (e.total_seconds ?? 0),
     0,
   );
-
   const combinedText =
     combinedSeconds >= 3600
       ? `${Math.floor(combinedSeconds / 3600)}h ${Math.floor(
