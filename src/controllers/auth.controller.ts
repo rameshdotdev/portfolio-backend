ae53f05010f74d7af4f41a1e03b2e00f4483844f
rameshdotdev <ramesh2041999@gmail.com>
2026-06-09 10:06:15 +0530

    auth fixes

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -7,17 +7,20 @@ export const loginAdmin = async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email }).select("+password");
+
   if (!user) {
     return res.status(401).json({ message: "Invalid email or password" });
   }
 
   const isMatch = await comparePassword(password, user.password);
+
   if (!isMatch) {
     return res.status(401).json({ message: "Invalid email or password" });
   }
 
   const token = generateToken(user._id.toString());
 
+  // Cookie
   res.cookie("auth_token", token, {
     httpOnly: true,
     secure: true,
@@ -27,7 +30,11 @@ export const loginAdmin = async (req: Request, res: Response) => {
     maxAge: 8 * 60 * 60 * 1000,
   });
 
-  return res.send(user);
+  // Also send token in response
+  return res.json({
+    user,
+    token,
+  });
 };
 
 export const logout = async (req: Request, res: Response) => {
